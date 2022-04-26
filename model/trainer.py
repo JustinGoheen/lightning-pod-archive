e48310e6fd5c43b3c3452f0efba8f070430d34d8
Justin G <26209687+JustinGoheen@users.noreply.github.com>
2022-04-26 13:49:53 -0400

    set fast_dev_run=True

--- model/trainer.py
+++ model/trainer.py
@@ -1,6 +1,7 @@
 # code example is from:
 # https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
 
+from asyncio import FastChildWatcher
 import os
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
@@ -11,7 +12,7 @@ from .model import LitModel
 if __name__ == "__main__":
 
     train_loader = DataLoader(MNIST(os.getcwd(), download=True, transform=transforms.ToTensor()))
-    trainer = pl.Trainer(max_epochs=1)
+    trainer = pl.Trainer(max_epochs=1, fast_dev_run=True)
     model = LitModel()
 
-    trainer.fit(model, train_dataloaders=train_loader)
+    trainer.fit(model, train_dataloaders=train_loader, )
