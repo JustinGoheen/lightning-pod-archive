9c46b83a8915e9d85b496a66bc9d621552f73b12
Justin G <26209687+JustinGoheen@users.noreply.github.com>
2022-04-26 21:17:23 -0400

    add install_requires

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
-import setuptools
+from setuptools import setup
+from setuptools import find_packages
 
-setuptools.setup(
+setup(
     name="lightning-pod",
     version="0.0.1",
     description="A robust PyTorch Lightning research environment",
-    url="",
+    url="https://github.com/JustinGoheen/lightning-pod",
     author="Justin Goheen",
-    install_requires=[""],
+    license="MIT",
+    install_requires=[
+        "pytorch-lightning",
+        "lightning-transformers",
+        "lightning-grid",
+        "lightning-bolts",
+        "lightning-flash",
+        "torchvision",
+        "torchaudio",
+        "torchtext",
+        "datasets",
+        "plotly",
+        "matplotlib",
+        "sympy",
+        "black",
+        "mypy",
+        "ipywidgets",
+        "easy-sphinx @ https://github.com/JustinGoheen/lightning-pod",
+    ],
     author_email="",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
 )
