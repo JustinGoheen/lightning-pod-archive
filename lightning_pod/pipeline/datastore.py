0e807d356dd8d8fedf0ce3f7f7bfc26823d7f2aa
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-04-30 22:23:59 -0400

    implement datastore.py

--- lightning_pod/pipeline/datastore.py
+++ lightning_pod/pipeline/datastore.py
@@ -1,3 +1,10 @@
-# TODO
-# create script to push to Grid datastore
-# this feature will be optional and not automated
+import os
+import acquisition
+
+if __name__ == "__main__":
+
+    rootpath = os.getcwd()
+    datapath = "".join([rootpath, "/", "data", "/", "cache"])
+    dataset = acquisition.get_data(return_loader=False)
+    storename = dataset.__class__.__name__
+    os.system(f"grid datastore create {datapath} --name {storename}")
