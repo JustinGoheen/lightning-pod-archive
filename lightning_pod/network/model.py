196bd2c857fde9ab1fd849c8201d56f0ffdf0be2
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-05-02 16:15:33 -0400

    add working example

--- lightning_pod/network/model.py
+++ lightning_pod/network/model.py
@@ -9,6 +9,7 @@ import pytorch_lightning as pl
 
 class Encoder(nn.Module):
     def __init__(self):
+        super().__init__()
         self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
 
     def forward(self, x):
@@ -17,6 +18,7 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
     def __init__(self):
+        super().__init__()
         self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
 
     def forward(self, x):
@@ -34,8 +36,8 @@ class LitModel(pl.LightningModule):
         x, y = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
+        y_hat = self.decoder(z)
+        loss = F.mse_loss(y_hat, x)
         return loss
 
     def configure_optimizers(self):
--- lightning_pod/network/trainer.py
+++ lightning_pod/network/trainer.py
@@ -1,18 +1,18 @@
 # code example is from:
 # https://pytorch-lightning.readthedocs.io/en/latest/model/train_model_basic.html
 
-import os
+
 import pytorch_lightning as pl
 from lightning_pod.network.model import LitModel, Encoder, Decoder
 from lightning_pod.pipeline.acquisition import get_data
 
 if __name__ == "__main__":
 
-    # data
+    #  get dataloader
     train_loader = get_data(return_loader=True)
-    # trainer
+    # trainer https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#trainer-flags
     trainer = pl.Trainer(max_epochs=1, fast_dev_run=True)
-    # model
+    # model https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#lightningmodule-api
     autoencoder = LitModel(Encoder(), Decoder())
-    # train model
+    # train model https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#fit
     trainer.fit(model=autoencoder, train_dataloaders=train_loader)
