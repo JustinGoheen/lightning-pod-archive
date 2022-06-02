bd71baf55c9b553d676114f986b6c34dfada039e
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-06-02 12:55:15 -0400

    add PyTorchProfile; add save splits

--- lightning_pod/network/static_trainer.py
+++ lightning_pod/network/static_trainer.py
@@ -1,8 +1,10 @@
 import os
+import torch
 from pathlib import Path
+from torch.utils.data import TensorDataset
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.profiler import SimpleProfiler
+from pytorch_lightning.profiler import PyTorchProfiler
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from lightning_pod.network.module import LitModel
 from lightning_pod.pipeline.datamodule import LitDataModule
@@ -19,9 +21,9 @@ if __name__ == "__main__":
     logs_dir = os.path.join(PROJECTPATH, "logs")
     logger = TensorBoardLogger(logs_dir, name="lightning_logs")
     # SET PROFILER
-    # https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.profiler.SimpleProfiler.html#simpleprofiler
+    # https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.profiler.PyTorchProfiler.html#pytorchprofiler
     profile_dir = os.path.join(logs_dir, "profiler")
-    profiler = SimpleProfiler(dirpath=profile_dir, filename="profiler", extended=True)
+    profiler = PyTorchProfiler(dirpath=profile_dir, filename="profiler")
     # SET CHECKPOINT CALLBACK
     # https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.callbacks.ModelCheckpoint.html#modelcheckpoint
     chkpt_dir = os.path.join(PROJECTPATH, "models", "checkpoints")
@@ -40,8 +42,9 @@ if __name__ == "__main__":
     # SET TRAINER
     # https://pytorch-lightning.readthedocs.io/en/latest/common/trainer.html
     trainer = Trainer(
-        max_epochs=5,
-        limit_train_batches=0.10,  # use only x% of training samples
+        max_epochs=100,
+        min_epochs=None,
+        limit_train_batches=1.0,  # use only x% of training samples; 1.0  is 100% of batch
         accelerator="auto",
         devices="auto",
         deterministic=True,  # for reproducibility
@@ -67,7 +70,6 @@ if __name__ == "__main__":
         check_val_every_n_epoch=1,
         fast_dev_run=False,
         accumulate_grad_batches=None,
-        min_epochs=None,
         max_steps=-1,
         min_steps=None,
         max_time=None,
@@ -89,7 +91,7 @@ if __name__ == "__main__":
         replace_sampler_ddp=True,
         detect_anomaly=False,
         auto_scale_batch_size=False,
-        amp_backend="negative",  # amp is "automatic mixed precision"
+        amp_backend="native",  # amp is "automatic mixed precision"
         amp_level=None,
         move_metrics_to_cpu=False,
         multiple_trainloader_mode="max_size_cycle",
@@ -103,3 +105,19 @@ if __name__ == "__main__":
     modelpath = os.path.join(pretrained_dir, "model.onnx")
     input_sample = datamodule.train_data.dataset[0][0]
     model.to_onnx(modelpath, input_sample=input_sample, export_params=True)
+    # PREDICT
+    predictions = trainer.predict(model, datamodule.val_dataloader())
+    # EXPORT PREDICTIONS
+    predictions = torch.vstack(predictions)
+    predictions = TensorDataset(predictions)
+    predictions_dir = os.path.join(PROJECTPATH, "data", "predictions")
+    prediction_fname = os.path.join(predictions_dir, "predictions.pt")
+    torch.save(predictions, prediction_fname)
+    # EXPORT ALL DATA SPLITS FOR REPRODUCIBILITY
+    split_dir = os.path.join(PROJECTPATH, "data", "training_split")
+    train_split_fname = os.path.join(split_dir, "train.pt")
+    test_split_fname = os.path.join(split_dir, "test.pt")
+    val_split_fname = os.path.join(split_dir, "val.pt")
+    torch.save(datamodule.train_data.dataset, train_split_fname)
+    torch.save(datamodule.test_data.dataset, test_split_fname)
+    torch.save(datamodule.val_data.dataset, val_split_fname)
